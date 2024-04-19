ec2a7b5278ee6761f99655c35fe55b4c942af2c3
momin-riyadh <mominriyadh@gmail.com>
2024-04-19 11:21:56 +0600

    fix and deploy projects page typography

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,7 +9,7 @@ export default function Projects() {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-6 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-6 md:text-5xl md:leading-6">
+          <h1 className="text-2xl font-extrabold leading-6 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-6 md:text-5xl md:leading-6">
             Projects
           </h1>
           <p className="text-lg leading-5 text-gray-500 dark:text-gray-400">
