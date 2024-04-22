e75d43cf021b7211fe93384dc3382a3f75ab2acd
Momin Riyadh <mominriyadh@gmail.com>
2024-04-22 12:40:14 +0600

    fix and deploy  project feature

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,21 +7,19 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Portfolio Template',
+    description: `A portfolio template for your personal website, built with documentation, working contact form and mobile first, technology stacks are node express javascript and ejs template engine`,
+    imgSrc: '/static/images/portfolio.jpg',
+    href: 'https://portfolio-af2r.onrender.com',
   },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+  // {
+  //   title: 'The Time Machine',
+  //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+  //   to the desired date and press "Go". No more worrying about lost keys or
+  //   forgotten headphones with this simple yet affordable solution.`,
+  //   imgSrc: '/static/images/time-machine.jpg',
+  //   href: '/blog/the-time-machine',
+  // },
 ]
 
 export default projectsData
